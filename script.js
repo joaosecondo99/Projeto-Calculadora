19e85984a064798c7244a31f630429ed8bcb57e6
JoaoSecondo <js.programadorbr@gmail.com>
2026-06-01 10:48:28 -0300

    trazendo as funcionalidades de: trocar de tema, botão de copiar para area de transferencias e limpar campos após resultado

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
     })
 })
 
-// Ao clicar no botão de limpar (C), apaga tudo do input e coloca o foco de volta nele para o usuário continuar digitando
+// Ao clicar no botão de limpar (C), apaga tudo do input de entrada e resultado, seta as classes caso tenha dado erro e coloca o foco de volta nele para o usuário continuar digitando
 
 document.getElementById('clear').addEventListener('click', function () {
     input.value = '';
+    resultInput.value = '';
+    resultInput.classList.remove('error');
     input.focus();
 })
 
@@ -49,7 +51,44 @@ input.addEventListener('keydown', function (ev) {
 document.getElementById('equal').addEventListener('click', calculate)
 
 function calculate() {
+    // Tratando sobre erro de expressão inválida na hora de calcular.
+    // O caminho é -> coloca no input de resultado a mensagem ERROR e adicionar a classList para pintar o fundo de vermelho.
+    // Caso a expressão esteja ok, as informações são substituidas pelo resultado e tira o fundo vermelho.
+    resultInput.value = 'ERROR';
+    resultInput.classList.add('error');
+
     // O eval() é uma função nativa do JavaScript que executa uma string como código. No caso da calculadora ela pega a String do input (Ex.: 2 + 2) e já interpreta o código e realiza o código. **NÃO USAR EM PROJETOS REAIS pois abre brexa para scripts maliciosos.
     const result = eval(input.value);
+
     resultInput.value = result;    
+    resultInput.classList.remove('error');
 }
+
+document.getElementById('themeSwitcher').addEventListener('click', function () {
+    if (main.dataset.theme == 'dark') {
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91')
+        main.dataset.theme = 'dark'
+    }
+})
+
+document.getElementById('copyToClipboard').addEventListener('click', function (ev) {
+    const button = ev.currentTarget;
+
+    if (button.innerText === 'Copy') {
+        button.innerText = 'Copied!';
+        button.classList.add('success');
+        window.navigator.clipboard.writeText(resultInput.value);
+    } else {
+        button.innerText = 'Copy';
+        button.classList.remove('success');
+    }
+})
